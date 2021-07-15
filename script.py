1341dc416e897b975c97eff21f481a9c3074678e
Rakhi-09 <82076954+Rakhi-09@users.noreply.github.com>
2021-07-15 14:16:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,13 @@ import subprocess
 
 import os
 # setting environment variables
+print("Setting env variables")
 os.environ['USER1'] = 'Rohan'
 os.environ['USER2'] = 'Rahul'
 os.environ['USER3'] = 'Rakhi'
 
 # calling shellscript
+print("Calling script.sh")
 subprocess.call(['bash','./script.sh'])
 
 
