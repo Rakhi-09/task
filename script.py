be0d2854796549825d23bf12bbaaac5f52c32450
Rakhi-09 <82076954+Rakhi-09@users.noreply.github.com>
2021-07-15 14:06:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
 import subprocess
-import shlex
+
 import os
+# setting environment variables
 os.environ['USER1'] = 'Rohan'
 os.environ['USER2'] = 'Rahul'
 os.environ['USER3'] = 'Rakhi'
-subprocess.call(shlex.split('./script.sh USER1 USER2 USER3'))
+
+# calling shellscript
+subprocess.call(['bash','./script.sh'])
+
 
 
 
