43d66fb14699e1c7f6d7221503a6f54696e9da45
Rakhi-09 <82076954+Rakhi-09@users.noreply.github.com>
2021-07-15 13:50:48 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
+import subprocess
+import shlex
 import os
+os.environ['USER1'] = 'Rohan'
+os.environ['USER2'] = 'Rahul'
+os.environ['USER3'] = 'Rakhi'
+subprocess.call(shlex.split('./script.sh USER1 USER2 USER3'))
+
+
+
 print(os.environ['HOME'])
 
 print("Hello world")
