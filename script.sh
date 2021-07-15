0c08048a5bce8fe4851dd22eed73805f9278b4e9
Rakhi-09 <82076954+Rakhi-09@users.noreply.github.com>
2021-07-15 14:20:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-echo "what is your name?"
-read PERSON
-echo "Hello,$PERSON"
+# echo "what is your name?"
+# read PERSON
+# echo "Hello,$PERSON"
 echo "NAME=$NAME"
 # printing env variables
+echo " Env var from Python script"
 echo "USER1=$USER1"
 echo "USER2=$USER2"
 echo "USER3=$USER3"
